b2c2952f715281510709637c4591543f75a1399c
Wh1isper <9573586@qq.com>
2023-01-06 17:29:35 +0800

    add permissions

--- fint_core/auth.py
+++ fint_core/auth.py
@@ -1,9 +1,11 @@
 #  Copyright (c) 2023 actfint
 #  Licensed under the BSD 3-Clause License
 #  Created by @Wh1isper 2023/1/4
+from typing import Any, Dict, Optional
 
 from pydantic import BaseModel
 
 
 class User(BaseModel):
     user_id: str
+    permissions: Optional[Dict[str, Any]]
